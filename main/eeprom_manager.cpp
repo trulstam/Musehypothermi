eefc351324206ce3e21e0856d27604785c30c3c4
trulstam <127051515+trulstam@users.noreply.github.com>
2025-10-08 20:50:20 +0200

    Consolidate EEPROM safety helpers for asymmetric PID

--- main/eeprom_manager.cpp
+++ main/eeprom_manager.cpp
@@ -1,10 +1,24 @@
 #include "eeprom_manager.h"
-#include <EEPROM.h>
 
-// Initiering av static const
+namespace {
+constexpr float kDefaultHeatingKp = 2.0f;
+constexpr float kDefaultHeatingKi = 0.5f;
+constexpr float kDefaultHeatingKd = 1.0f;
+
+constexpr float kDefaultCoolingKp = 1.5f;
+constexpr float kDefaultCoolingKi = 0.3f;
+constexpr float kDefaultCoolingKd = 0.8f;
+
+constexpr float kDefaultTargetTemp = 37.0f;
+constexpr float kDefaultMaxOutput = 35.0f;
+constexpr float kDefaultCoolingRate = 2.0f;
+constexpr float kDefaultDeadband = 0.5f;
+constexpr float kDefaultSafetyMargin = 2.0f;
+constexpr int kDefaultDebugLevel = 0;
+constexpr int kDefaultFailsafeTimeout = 5000;
+}  // namespace
+
 const uint32_t EEPROMManager::MAGIC_NUMBER = 0xDEADBEEF;
-
-// Ingen EEPROM.begin() kreves for Uno R4 Minima
 
 bool EEPROMManager::begin() {
     if (!isMagicNumberValid()) {
@@ -14,15 +28,35 @@ bool EEPROMManager::begin() {
 }
 
 void EEPROMManager::savePIDParams(float kp, float ki, float kd) {
+    saveHeatingPIDParams(kp, ki, kd);
+}
+
+void EEPROMManager::loadPIDParams(float &kp, float &ki, float &kd) {
+    loadHeatingPIDParams(kp, ki, kd);
+}
+
+void EEPROMManager::saveHeatingPIDParams(float kp, float ki, float kd) {
     EEPROM.put(addrKp, kp);
     EEPROM.put(addrKi, ki);
     EEPROM.put(addrKd, kd);
 }
 
-void EEPROMManager::loadPIDParams(float &kp, float &ki, float &kd) {
+void EEPROMManager::loadHeatingPIDParams(float &kp, float &ki, float &kd) {
     EEPROM.get(addrKp, kp);
     EEPROM.get(addrKi, ki);
     EEPROM.get(addrKd, kd);
+}
+
+void EEPROMManager::saveCoolingPIDParams(float kp, float ki, float kd) {
+    EEPROM.put(addrCoolingKp, kp);
+    EEPROM.put(addrCoolingKi, ki);
+    EEPROM.put(addrCoolingKd, kd);
+}
+
+void EEPROMManager::loadCoolingPIDParams(float &kp, float &ki, float &kd) {
+    EEPROM.get(addrCoolingKp, kp);
+    EEPROM.get(addrCoolingKi, ki);
+    EEPROM.get(addrCoolingKd, kd);
 }
 
 void EEPROMManager::saveTargetTemp(float temp) {
@@ -34,11 +68,74 @@ void EEPROMManager::loadTargetTemp(float &temp) {
 }
 
 void EEPROMManager::saveMaxOutput(float maxOutput) {
-    EEPROM.put(addrMaxOutput, maxOutput);
+    saveHeatingMaxOutput(maxOutput);
+    saveCoolingMaxOutput(maxOutput);
 }
 
 void EEPROMManager::loadMaxOutput(float &maxOutput) {
-    EEPROM.get(addrMaxOutput, maxOutput);
+    loadHeatingMaxOutput(maxOutput);
+}
+
+void EEPROMManager::saveHeatingMaxOutput(float maxOutput) {
+    EEPROM.put(addrHeatingMaxOutput, maxOutput);
+}
+
+void EEPROMManager::loadHeatingMaxOutput(float &maxOutput) {
+    EEPROM.get(addrHeatingMaxOutput, maxOutput);
+}
+
+void EEPROMManager::saveCoolingMaxOutput(float maxOutput) {
+    EEPROM.put(addrCoolingMaxOutput, maxOutput);
+}
+
+void EEPROMManager::loadCoolingMaxOutput(float &maxOutput) {
+    EEPROM.get(addrCoolingMaxOutput, maxOutput);
+}
+
+void EEPROMManager::saveOutputLimits(const OutputLimits &limits) {
+    saveHeatingMaxOutput(limits.heatingPercent);
+    saveCoolingMaxOutput(limits.coolingPercent);
+}
+
+void EEPROMManager::loadOutputLimits(OutputLimits &limits) {
+    loadHeatingMaxOutput(limits.heatingPercent);
+    loadCoolingMaxOutput(limits.coolingPercent);
+}
+
+void EEPROMManager::saveCoolingRateLimit(float rate) {
+    EEPROM.put(addrCoolingRateLimit, rate);
+}
+
+void EEPROMManager::loadCoolingRateLimit(float &rate) {
+    EEPROM.get(addrCoolingRateLimit, rate);
+}
+
+void EEPROMManager::saveDeadband(float deadband) {
+    EEPROM.put(addrDeadband, deadband);
+}
+
+void EEPROMManager::loadDeadband(float &deadband) {
+    EEPROM.get(addrDeadband, deadband);
+}
+
+void EEPROMManager::saveSafetyMargin(float margin) {
+    EEPROM.put(addrSafetyMargin, margin);
+}
+
+void EEPROMManager::loadSafetyMargin(float &margin) {
+    EEPROM.get(addrSafetyMargin, margin);
+}
+
+void EEPROMManager::saveSafetySettings(const SafetySettings &settings) {
+    saveCoolingRateLimit(settings.coolingRateLimit);
+    saveDeadband(settings.deadband);
+    saveSafetyMargin(settings.safetyMargin);
+}
+
+void EEPROMManager::loadSafetySettings(SafetySettings &settings) {
+    loadCoolingRateLimit(settings.coolingRateLimit);
+    loadDeadband(settings.deadband);
+    loadSafetyMargin(settings.safetyMargin);
 }
 
 void EEPROMManager::saveDebugLevel(int debugLevel) {
@@ -58,14 +155,22 @@ void EEPROMManager::loadFailsafeTimeout(int &timeout) {
 }
 
 bool EEPROMManager::factoryReset() {
-    savePIDParams(2.0f, 0.5f, 1.0f);
-    saveTargetTemp(37.0f);
-    saveMaxOutput(20.0f);
-    saveDebugLevel(0);
-    saveFailsafeTimeout(5000);
+    saveHeatingPIDParams(kDefaultHeatingKp, kDefaultHeatingKi, kDefaultHeatingKd);
+    saveCoolingPIDParams(kDefaultCoolingKp, kDefaultCoolingKi, kDefaultCoolingKd);
+    saveTargetTemp(kDefaultTargetTemp);
+    saveMaxOutput(kDefaultMaxOutput);
+
+    SafetySettings safety{
+        kDefaultCoolingRate,
+        kDefaultDeadband,
+        kDefaultSafetyMargin,
+    };
+    saveSafetySettings(safety);
+
+    saveDebugLevel(kDefaultDebugLevel);
+    saveFailsafeTimeout(kDefaultFailsafeTimeout);
 
     saveMagicNumber();
-
     return true;
 }
 
@@ -73,8 +178,8 @@ void EEPROMManager::saveMagicNumber() {
     EEPROM.put(addrMagic, MAGIC_NUMBER);
 }
 
-bool EEPROMManager::isMagicNumberValid() {
-    uint32_t magic;
+bool EEPROMManager::isMagicNumberValid() const {
+    uint32_t magic = 0;
     EEPROM.get(addrMagic, magic);
-    return (magic == MAGIC_NUMBER);
+    return magic == MAGIC_NUMBER;
 }
--- main/eeprom_manager.h
+++ main/eeprom_manager.h
@@ -6,16 +6,54 @@
 
 class EEPROMManager {
 public:
+    struct OutputLimits {
+        float heatingPercent;
+        float coolingPercent;
+    };
+
+    struct SafetySettings {
+        float coolingRateLimit;
+        float deadband;
+        float safetyMargin;
+    };
+
     bool begin();
 
     void savePIDParams(float kp, float ki, float kd);
     void loadPIDParams(float &kp, float &ki, float &kd);
+
+    void saveHeatingPIDParams(float kp, float ki, float kd);
+    void loadHeatingPIDParams(float &kp, float &ki, float &kd);
+
+    void saveCoolingPIDParams(float kp, float ki, float kd);
+    void loadCoolingPIDParams(float &kp, float &ki, float &kd);
 
     void saveTargetTemp(float temp);
     void loadTargetTemp(float &temp);
 
     void saveMaxOutput(float maxOutput);
     void loadMaxOutput(float &maxOutput);
+
+    void saveHeatingMaxOutput(float maxOutput);
+    void loadHeatingMaxOutput(float &maxOutput);
+
+    void saveCoolingMaxOutput(float maxOutput);
+    void loadCoolingMaxOutput(float &maxOutput);
+
+    void saveOutputLimits(const OutputLimits &limits);
+    void loadOutputLimits(OutputLimits &limits);
+
+    void saveCoolingRateLimit(float rate);
+    void loadCoolingRateLimit(float &rate);
+
+    void saveDeadband(float deadband);
+    void loadDeadband(float &deadband);
+
+    void saveSafetyMargin(float margin);
+    void loadSafetyMargin(float &margin);
+
+    void saveSafetySettings(const SafetySettings &settings);
+    void loadSafetySettings(SafetySettings &settings);
 
     void saveDebugLevel(int debugLevel);
     void loadDebugLevel(int &debugLevel);
@@ -30,16 +68,22 @@ private:
     static const int addrKi = addrKp + sizeof(float);
     static const int addrKd = addrKi + sizeof(float);
     static const int addrTargetTemp = addrKd + sizeof(float);
-    static const int addrMaxOutput = addrTargetTemp + sizeof(float);
-    static const int addrDebugLevel = addrMaxOutput + sizeof(float);
+    static const int addrHeatingMaxOutput = addrTargetTemp + sizeof(float);
+    static const int addrCoolingMaxOutput = addrHeatingMaxOutput + sizeof(float);
+    static const int addrDebugLevel = addrCoolingMaxOutput + sizeof(float);
     static const int addrFailsafeTimeout = addrDebugLevel + sizeof(int);
     static const int addrMagic = addrFailsafeTimeout + sizeof(int);
+    static const int addrCoolingKp = addrMagic + sizeof(uint32_t);
+    static const int addrCoolingKi = addrCoolingKp + sizeof(float);
+    static const int addrCoolingKd = addrCoolingKi + sizeof(float);
+    static const int addrCoolingRateLimit = addrCoolingKd + sizeof(float);
+    static const int addrDeadband = addrCoolingRateLimit + sizeof(float);
+    static const int addrSafetyMargin = addrDeadband + sizeof(float);
 
-    // Bare deklarasjon, ikke initialisering her!
     static const uint32_t MAGIC_NUMBER;
 
     void saveMagicNumber();
-    bool isMagicNumberValid();
+    bool isMagicNumberValid() const;
 };
 
-#endif // EEPROM_MANAGER_H
+#endif  // EEPROM_MANAGER_H
